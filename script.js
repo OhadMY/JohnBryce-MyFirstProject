873c2fa17a7520cea15fce3e9f5596fd8190b3cd
Ohad <Ohad.Mor.Yosef@gmail.com>
2021-02-12 03:40:19 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Grab Elements
+const form = document.querySelector("form");
+const taskContent = document.querySelector("#taskcontent");
+const taskDate = document.querySelector("#date");
+const taskTime = document.querySelector("#time");
+const tasksList = document.querySelector("#tasks");
+
+// Creating tasks array
+let tasksArray = [];
